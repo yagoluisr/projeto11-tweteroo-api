3c4be0e19018f33671d6dfe656074b587bd8a689
yagoluis <yagoluisroberto@gmail.com>
2022-08-27 10:00:40 -0300

    Implementado post sign-up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import express from 'express';
 
 const server = express();
+server.use(express.json());
+
+const user = [
+    {
+        username: 'bobesponja', 
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+    }
+]
+
+const tweet = [
+    {
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
 
 
-
+server.post('/sign-up', (req, res) => {
+    const userLogin = req.body;
+    
+    user.push(userLogin);
+    res.send('OK');
+});
 
 
 
